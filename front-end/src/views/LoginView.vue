ad99c128941e2144104be7c4cacd6b02af416c85
Soh Thiam Hing <daffodilistic@gmail.com>
2022-10-20 00:51:46 +0800

    [feature(oauth)] Basic login function done

--- front-end/src/views/LoginView.vue
+++ front-end/src/views/LoginView.vue
@@ -5,6 +5,13 @@
         <v-card class="mx-auto" max-width="344">
           <v-card-actions>
             <v-col class="text-center">
+              <template v-if="email != null">
+                <h3>Hello, {{ email }}</h3>
+              </template>
+              <template v-else>
+                <h3>Please sign in</h3>
+              </template>
+              <br />
               <v-btn color="cyan accent-4" dark @click="signUp"
                 >Login / Sign Up</v-btn
               >
@@ -28,11 +35,66 @@ export default {
   },
   data() {
     return {
-      username: "",
-      password: "",
+      email: null,
+      keycloak: null,
     };
   },
-  mounted() {},
+  mounted() {
+    let initOptions = {
+      url: `${process.env.VUE_APP_ENDPOINT_OAUTH}`,
+      realm: "IamOrderMatching",
+      clientId: "iam-ordermatching",
+      onLoad: "check-sso",
+    };
+
+    this.keycloak = new Keycloak(initOptions);
+
+    this.keycloak
+      .init({
+        onLoad: initOptions.onLoad,
+        // redirectUri: `${process.env.VUE_APP_ENDPOINT_OAUTH_REDIRECT_URI}`,
+      })
+      .then((auth) => {
+        if (!auth) {
+          console.info("error");
+        } else {
+          console.info("Authenticated");
+          console.info(this.keycloak.tokenParsed);
+          console.info("keycloak.token:" + this.keycloak.token);
+          console.info("keycloak.refreshToken" + this.keycloak.refreshToken);
+          // localStorage.setItem("vue-token", this.keycloak.token);
+          // localStorage.setItem("vue-refresh-token", this.keycloak.refreshToken);
+          this.email = this.keycloak.tokenParsed.email;
+        }
+
+        //Token Refresh
+        setInterval(() => {
+          this.keycloak
+            .updateToken(70)
+            .then((refreshed) => {
+              if (refreshed) {
+                console.info("Token refreshed" + refreshed);
+              } else {
+                console.warn(
+                  "Token not refreshed, valid for " +
+                    Math.round(
+                      this.keycloak.tokenParsed.exp +
+                        this.keycloak.timeSkew -
+                        new Date().getTime() / 1000
+                    ) +
+                    " seconds"
+                );
+              }
+            })
+            .catch(() => {
+              console.error("Failed to refresh token");
+            });
+        }, 6000);
+      })
+      .catch(() => {
+        console.error("Authenticated Failed");
+      });
+  },
   methods: {
     ...mapMutations({
       saveUserData: "saveUserState",
@@ -57,59 +119,7 @@ export default {
       }
     },
     signUp() {
-      let initOptions = {
-        url: `${process.env.VUE_APP_ENDPOINT_ACCOUNTS}`,
-        realm: "IamOrderMatching",
-        clientId: "iam-ordermatching",
-        onLoad: "login-required",
-      };
-
-      let keycloak = new Keycloak(initOptions);
-
-      keycloak
-        .init({
-          onLoad: initOptions.onLoad,
-          redirectUri: `${process.env.VUE_APP_ENDPOINT_OAUTH_REDIRECT_URI}`,
-        })
-        .then((auth) => {
-          if (!auth) {
-            window.location.reload();
-            console.log.info("error");
-          } else {
-            console.log.info("Authenticated");
-            console.log.info("keycloak.token:" + keycloak.token);
-            console.log.info("keycloak.refreshToken" + keycloak.refreshToken);
-            localStorage.setItem("vue-token", keycloak.token);
-            localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
-          }
-
-          //Token Refresh
-          setInterval(() => {
-            keycloak
-              .updateToken(70)
-              .then((refreshed) => {
-                if (refreshed) {
-                  console.log.info("Token refreshed" + refreshed);
-                } else {
-                  console.log.warn(
-                    "Token not refreshed, valid for " +
-                      Math.round(
-                        keycloak.tokenParsed.exp +
-                          keycloak.timeSkew -
-                          new Date().getTime() / 1000
-                      ) +
-                      " seconds"
-                  );
-                }
-              })
-              .catch(() => {
-                console.log.error("Failed to refresh token");
-              });
-          }, 6000);
-        })
-        .catch(() => {
-          console.log.error("Authenticated Failed");
-        });
+      this.keycloak.login();
     },
   },
 };
